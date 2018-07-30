662c81c374255b52110ce75fcab1ae16ab4f4800
Jerlilson Bezerra da Silva <jerlilsonbezerradasilva@MacBook-Pro-de-Jerlilson.local>
2018-07-29 20:00:34 -0400

    tentativa de reservar o laboratorio selecionado
    Signed-off-by: Jerlilson Bezerra da Silva <jerlilsonbezerradasilva@MacBook-Pro-de-Jerlilson.local>

--- src/pages/Laboratorios.vue
+++ src/pages/Laboratorios.vue
@@ -8,16 +8,21 @@
       :isSelected="isSelected(lab._id)"
       @click.native="select(lab._id)"
     />
-    <button v-on:click="warn('Form cannot be submitted yet.', $event)">Enviar</button>
+    <observacao></observacao>
+    <button type="button" class="btn" v-on:click="reservar">Reservar</button>
+
+    <!-- <button v-on:click="warn('Form cannot be submitted yet.', $event)">Enviar</button> -->
   </div>
 </template>
 <script>
 import api from "../services/api";
 import LabItem from "../components/LabItem";
+import Observacao from "../components/Observacao";
 
 export default {
   components: {
-    LabItem
+    LabItem,
+    Observacao
   },
   async mounted() {
     const response = await api.get("/administration/");
@@ -36,11 +41,23 @@ export default {
       this.idSelecionado = id;
       console.log(this.idSelecionado);
     },
-
     isSelected(id) {
       return this.idSelecionado === id;
     },
-  },
+    reservar(){
+        console.log("Tudo normales com o token:", localStorage.token);
+        console.log("Olha o id selecionado:", this.idSelecionado);
+        const self = this;
+        
+        api.post('/administration/', {"laboratory" : self.idSelecionado}, {headers : {Authorization: `Bearer ${localStorage.token}`}}
+        ).then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
     
